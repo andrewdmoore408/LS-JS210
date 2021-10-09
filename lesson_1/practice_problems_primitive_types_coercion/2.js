8329f092594b0030cfd0ee1b41049b3b692bd17e
Andrew Moore <andrewdmoore84@gmail.com>
2021-10-08 17:02:21 -0700

    Add code/responses for problems 1-5

---
+++ lesson_1/practice_problems_primitive_types_coercion/2.js
@@ -0,0 +1,6 @@
+let x = '13';
+let y = 9;
+
+console.log(x * y);
+
+// Mathematical operators other than + are only defined for numbers in JS, so the value x points to will be coerced to a number and the numeric product of 13 and 9 will be returned.
---
+++ lesson_1/practice_problems_primitive_types_coercion/5.js
@@ -0,0 +1,6 @@
+let bool = true;
+
+console.log(bool.toString());
+
+let arr = [1, 2, 3];
+console.log(arr.toString());
