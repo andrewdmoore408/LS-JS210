70d3d1a5fc27d49d0fe54c439ef82ca30e418598
Andrew Moore <andrewdmoore84@gmail.com>
2021-11-09 18:48:23 -0500

    Add responses to practice problems

---
+++ lesson_6/practice_problem_strict_mode.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function createDeck() {
+  const SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"];
+  const RANKS = ["2", "3", "4", "5", "6", "7", "8", "9",
+    "10", "Jack", "Queen", "King", "Ace"];
+
+  const allCards = () => {
+    return SUITS.reduce((deck, suit) => {
+      RANKS.forEach(rank => deck.push(`${rank} of ${suit}`));
+      return deck;
+    }, []);
+  };
+
+  const deck = allCards();
+  shuffle(deck);
+
+  return deck;
+}
+
+function shuffle(deck) {
+  for (let counter = 0; counter < 400; counter += 1) {
+    const randomIndex1 = randomCardIndex();
+    const randomIndex2 = randomCardIndex();
+    const tempCard = deck[randomIndex1];
+    deck[randomIndex1] = deck[randomIndex2];
+    deck[randomIndex2] = tempCard;
+  }
+
+  function randomCardIndex() {
+    return Math.floor(Math.random() * deck.length);
+  }
+}
+
+console.log(createDeck());
